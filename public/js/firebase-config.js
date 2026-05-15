d5a7327d6cc74b68ea8731989dba3d06be23e3bf
leadwase <supportleadwase@gmail.com>
2026-05-15 15:35:19 +0100

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,13 +1,11 @@
-import { initializeApp }  from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth }        from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getFirestore }   from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-import { getStorage }     from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-const res    = await fetch('/api/config');
+const res = await fetch('/api/config');
 const config = await res.json();
 
 const _app = initializeApp(config);
 export const auth = getAuth(_app);
-export const db   = getFirestore(_app);
-export const storage = getStorage(_app);
-export const API  = '/api';
+export const db = getFirestore(_app);
+export const API = '/api';
